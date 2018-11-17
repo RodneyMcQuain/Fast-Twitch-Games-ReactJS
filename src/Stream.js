f5c7196d55f5276873a6de6672f5d3540b0f694a
Rodney McQuain <rodneymcquain95@gmail.com>
2018-11-17 17:02:28 -0500

    Added on stream hover feature
    Whenever a stream div is hovered over a smaller version of the title will be displayed after 750ms and will disappear when the div is no longer being hovered over.

--- src/Stream.js
+++ src/Stream.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function Stream(props) {
+/*Class that renders a stream div with the url, title,
+  thumbnail, display name, and viewers.*/
+class Stream extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hoverOpacity: "",
+      hoverFilter: "",
+    };
+
+    this.handleOnMouseEnter_hoverTitle = this.handleOnMouseEnter_hoverTitle.bind(this);
+    this.handleOnMouseLeave_hoverTitle = this.handleOnMouseLeave_hoverTitle.bind(this);
+  }
+
+  /*Renders a div with the url, title,
+    thumbnail, display name, and viewers.*/
+  render() {
     return (
-      <a href = { props.url } target = "_blank">
-        <div className="divToRemove">
-          <img src = { props.thumbnail }></img>
-          <h1>{ props.displayName }</h1>
-          <p>{ props.title }</p>
-          <p>&nbsp;- { props.viewers } viewers</p>
+      <a href = { this.props.url } target = "_blank">
+        <div className="divToRemove" onMouseEnter={this.handleOnMouseEnter_hoverTitle} onMouseLeave={this.handleOnMouseLeave_hoverTitle}>
+          <img src = { this.props.thumbnail }></img>
+          <h1>{ this.props.displayName }</h1>
+          <p>{ this.props.title }</p>
+          <p>&nbsp;- { this.props.viewers } viewers</p>
+          <div className="streamHoverParent" style={{"opacity": this.state.hoverOpacity, "filter": this.state.hoverFilter}}>
+            <div className="streamHoverChild">{ this.props.title }</div>
+          </div>
         </div>
       </a>
     );
+  }
+
+  /*When a stream div is hovered over display
+    a smaller verrsion of the title of the stream
+    after 750ms.*/
+  handleOnMouseEnter_hoverTitle() {
+    this.timerID = setTimeout(function() {
+      this.setState({hoverOpacity: ".85"});
+      this.setState({hoverFilter: "none"});
+    }.bind(this), 750);
+  }
+
+  /*When a stream div is no longer being hovered over
+    clear the timeout and hide the title.*/
+  handleOnMouseLeave_hoverTitle() {
+    clearTimeout(this.timerID);
+    this.setState({hoverOpacity: "0"});
+    this.setState({hoverFilter: "alpha(opacity = 0)"});
+  }
 }
 
 export default Stream;
